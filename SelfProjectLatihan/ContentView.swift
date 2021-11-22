e7580fe61dce0eb685bf41b2ab7f8a5251b78b73
Ahmad Dody <ahmad.ch@students.amikom.ac.id>
2021-11-22 13:41:36 +0700

    coba coba

--- SelfProjectLatihan/ContentView.swift
+++ SelfProjectLatihan/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                    .padding()
+            Text("Ahmad Dody")
+                .padding()
+
+        }
     }
 }
 
