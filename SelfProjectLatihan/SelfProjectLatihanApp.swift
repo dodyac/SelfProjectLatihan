cc5addadeedca08a2889ffdf8c505b00e0841864
Ahmad Dody <ahmad.ch@students.amikom.ac.id>
2021-11-22 13:39:12 +0700

    Initial Commit

---
+++ SelfProjectLatihan/SelfProjectLatihanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfProjectLatihanApp.swift
+//  SelfProjectLatihan
+//
+//  Created by mac pro on 22/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SelfProjectLatihanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
